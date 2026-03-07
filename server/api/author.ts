4f7f6550800ede1cd223fedd907eba4d4991b2f4
susowiki <boz6g4g9>
2026-03-07 19:21:00 +0100

    refactor: Update author entry content type from ID to 'authorV2'.

--- server/api/author.ts
+++ server/api/author.ts
@@ -10,7 +10,7 @@ export default defineEventHandler(async () => {
 
     try {
         const entries = await client.getEntries({
-            content_type: '7pionUCdi3UEuos10iCf2c',
+            content_type: 'authorV2',
             limit: 1,
             include: 2,
         })
